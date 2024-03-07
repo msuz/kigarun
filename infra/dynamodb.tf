b01762b700a0dddf259e69be178c76149e7e5a1c
msuz <suzukimasahiko.jp@gmail.com>
2024-03-08 07:28:21 +0900

    create dynamodb table

---
+++ infra/dynamodb.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 3
+  write_capacity = 3
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 2
+    read_capacity      = 2
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-GameScores"
+    Environment = "production"
+  }
+}
