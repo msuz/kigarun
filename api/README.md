5a09925218156cae95b0daa73cbe6e864eb643a4
msuz <suzukimasahiko.jp@gmail.com>
2024-03-17 18:13:48 +0900

    generated get_message.py and test code

--- api/README.md
+++ api/README.md
@@ -1,1 +1,37 @@
 # kigarun api
+
+## Environment
+
+* aws lambda function via api gateway
+* runtime: python 3.12
+
+## Unit Test
+
+```
+% cd /PATH/TO/KIGARUN/api/test
+% python3 -m unittest                    
+.
+----------------------------------------------------------------------
+Ran 1 test in 0.000s
+
+OK
+```
+
+## Deploy
+
+```
+% cd /PATH/TO/KIGARUN/api
+% vi get_message.py
+% zip get_message.zip get_message.py
+
+% cd /PATH/TO/KIGARUN/infra
+% terraform plan
+% terraform apply
+  Enter a value: yes
+```
+
+## Integration Test
+
+```
+(TBD)
+```
---
+++ api/test/test_get_message.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+import os
+import json
+
+
+# テスト対象のLambda関数があるディレクトリ（一つ上のディレクトリ）をsys.pathに追加
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from get_message import lambda_handler
+
+class TestGetMessageLambdaHandler(unittest.TestCase):
+    def test_lambda_handler(self):
+        """Lambda関数が期待通りのレスポンスを返すことをテストする"""
+        expected_response = {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': '{"status": "ok", "message": "hello world"}'
+        }
+        
+        # Lambda関数をテストするためのダミーのeventとcontextオブジェクトを渡します。
+        result = lambda_handler({}, {})
+        data = json.loads(result['body'])
+        self.assertEqual(result, expected_response)
+        self.assertEqual(data['status'], 'ok')
+        self.assertEqual(data['message'], 'hello world')
+
+if __name__ == '__main__':
+    unittest.main()
