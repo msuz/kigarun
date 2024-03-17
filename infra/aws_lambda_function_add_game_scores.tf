52bb82bd77bf3971920cf7d878361754aa23927a
msuz <suzukimasahiko.jp@gmail.com>
2024-03-17 23:55:33 +0900

    create lambda_function *_game_scores with temp .py

--- infra/aws_lambda_function_add_game_scores.tf
+++ infra/aws_lambda_function_add_game_scores.tf
@@ -1,27 +1,27 @@
-resource "aws_lambda_function" "get_message" {
-  function_name = "GetMessage"
+resource "aws_lambda_function" "add_game_scores" {
+  function_name = "AddGameScores"
   role          = aws_iam_role.api_lambda_execution_role.arn
-  handler       = "get_message.lambda_handler"
+  handler       = "add_game_scores.lambda_handler"
   runtime       = "python3.12"
 
   # 更新されたZIPファイルのパス
-  filename         = "${path.module}/../api/get_message.zip"
-  source_code_hash = filebase64sha256("${path.module}/../api/get_message.zip")
+  filename         = "${path.module}/../api/add_game_scores.zip"
+  source_code_hash = filebase64sha256("${path.module}/../api/add_game_scores.zip")
 }
 
 # Lambda関数のコードを含むZIPファイルを作成
 resource "null_resource" "lambda_zip" {
   triggers = {
-    file_checksum = filemd5("${path.module}/../api/get_message.py")
+    file_checksum = filemd5("${path.module}/../api/add_game_scores.py")
   }
 
   provisioner "local-exec" {
-    command = "cd ${path.module}/../api && zip get_message.zip get_message.py"
+    command = "cd ${path.module}/../api && zip add_game_scores.zip add_game_scores.py"
   }
 
   provisioner "local-exec" {
     when    = destroy
-    command = "rm -f ${path.module}/../api/get_message.zip"
+    command = "rm -f ${path.module}/../api/add_game_scores.zip"
   }
 }
 
@@ -29,7 +29,7 @@ resource "null_resource" "lambda_zip" {
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.get_message.function_name
+  function_name = aws_lambda_function.add_game_scores.function_name
   principal     = "apigateway.amazonaws.com"
   # API GatewayのARNを指定
   # source_arn = "arn:aws:execute-api:region:account-id:api-id/*/*/*"
